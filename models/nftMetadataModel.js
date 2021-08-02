32ea68b285f810ec6bf752604c9a9a33f4b46574
seantan1 <seantan374@gmail.com>
2021-08-02 15:03:12 +1000

    require uniqueValidator

--- models/nftMetadataModel.js
+++ models/nftMetadataModel.js
@@ -1,5 +1,6 @@
 // nftMetadataModel.js
 var mongoose = require('mongoose');
+var uniqueValidator = require('mongoose-unique-validator'); // unique validator
 // Setup schema
 var nftMetadataSchema = mongoose.Schema({
     // pair combination of game_id & item_id (e.g. if game_id = 001 and item_id = 010, game_id_item_id_pair would be 001_010)
