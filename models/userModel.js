35217331e6cae081c8df79feb4ac1d830efdf5c1
Nikita Phung <nikitaphung2000@gmail.com>
2021-08-19 11:46:21 +1000

    Updated userModel

--- models/userModel.js
+++ models/userModel.js
@@ -1,3 +1,5 @@
+const bcrypt = require('bcrypt')
+const saltRounds = 10;
 // userModel.js
 var mongoose = require('mongoose');
 var uniqueValidator = require('mongoose-unique-validator'); // unique validator
@@ -13,6 +15,28 @@ var userSchema = mongoose.Schema({
         required: true
     }
 });
+
+
+// Before saving, hash the password
+userSchema.pre('save', (next) => {
+    var user = this;
+    if(user.isModified("password_hash")) {
+        bcrypt.getSalt(saltRounds, (err, salt) => {
+            if(err){
+                return next(err)
+            }
+            bcrypt.hash(user.password_hash, salt, (err, hash) => {
+                if(err){
+                    return next(err)
+                }
+                user.password_hash = hash
+            })
+        })
+    } else {
+        next()
+    }
+  
+})
 userSchema.plugin(uniqueValidator); // unique validator
 // Export User model
 var User = module.exports = mongoose.model('user', userSchema);
