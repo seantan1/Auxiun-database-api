3c8e03869be77e69fe88bfb74372a402ae7b45a4
seantan1 <seantan374@gmail.com>
2021-09-07 12:48:12 +1000

    Update nftMetadataController.js

--- controllers/nftMetadataController.js
+++ controllers/nftMetadataController.js
@@ -68,31 +68,26 @@ exports.view = function (req, res) {
 };
 // Handle update nftMetadata info
 exports.update = function (req, res) {
-    if (req.body.apikey == process.env.PRIVATE_API_KEY) {
-        NftMetadata.findById(req.params.nftMetadata_id, function (err, nftMetadata) {
+    NftMetadata.findById(req.params.nftMetadata_id, function (err, nftMetadata) {
+        if (err)
+            res.send(err);
+        nftMetadata.game_id_item_id_pair = req.body.game_id + "_" + req.body.item_id;
+        nftMetadata.game_id = req.body.game_id;
+        nftMetadata.item_id = req.body.item_id;
+        nftMetadata.item_name = req.body.item_name;
+        nftMetadata.item_description = req.body.item_description;
+        nftMetadata.item_popularity = req.body.item_popularity;
+        nftMetadata.item_image = req.file.path;
+        // save the nftMetadata and check for errors
+        nftMetadata.save(function (err) {
             if (err)
-                res.send(err);
-            nftMetadata.game_id_item_id_pair = req.body.game_id + "_" + req.body.item_id;
-            nftMetadata.game_id = req.body.game_id;
-            nftMetadata.item_id = req.body.item_id;
-            nftMetadata.item_name = req.body.item_name;
-            nftMetadata.item_description = req.body.item_description;
-            nftMetadata.item_popularity = req.body.item_popularity;
-            nftMetadata.item_image = req.file.path;
-            // save the nftMetadata and check for errors
-            nftMetadata.save(function (err) {
-                if (err)
-                    res.json(err);
-                res.json({
-                    message: 'NFT Metadata info updated',
-                    data: nftMetadata
-                });
+                res.json(err);
+            res.json({
+                message: 'NFT Metadata info updated',
+                data: nftMetadata
             });
         });
-    }
-    else {
-        res.json('Not authorised');
-    }
+    });
 };
 // Handle delete donation
 exports.delete = function (req, res) {
@@ -123,7 +118,7 @@ exports.fetchNftMetadata = function (req, res) {
             if (nftMetadata.length == 0) {
                 res.json({
                     status: "error",
-                    message: 'NFT Metadata not found for '+req.params.game_id + "_" + req.params.item_id
+                    message: 'NFT Metadata not found for ' + req.params.game_id + "_" + req.params.item_id
                 });
             }
             else {
@@ -142,7 +137,7 @@ exports.fetchNFTMetadatasByGameId = function (req, res) {
             if (nftMetadata.length == 0) {
                 res.json({
                     status: "error",
-                    message: 'NFT Metadatas not found for game ID: '+ req.params.game_id
+                    message: 'NFT Metadatas not found for game ID: ' + req.params.game_id
                 });
             }
             else {
